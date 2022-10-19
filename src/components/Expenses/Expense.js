b07e99b5312332cc9afb49ceb5bab3f9bf7a33c9
Gazalrafi <grafi7485>
2022-10-19 19:26:19 +0530

    my evenet commit

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React,{useState} from "react";
 import ExpenseDate from './ExpenseDate.js';
 import './Expense.css';
 import ExpenseAmount from './ExpenseAmount.js';
 import Card from '../UI/Card.js';
 const Expense=(props)=>{
-   
+   const [newtitle,setTitle]=useState(props.title);
+   const clickHandler=()=>{
+    setTitle('new title');
+   }
     return(
         
         <Card className='expense-item'>
     
-        <div className='expense-item__title'>{props.title}</div>
+        <div className='expense-item__title'><h1>{newtitle}</h1></div>
         <ExpenseDate date={props.date} />
         <ExpenseAmount amount={props.amount} />
         <div className='expense-item__location'>{props.location}</div>
-
+        <button onClick={clickHandler}>Click here to change title</button>
         </Card>
     );
 }
