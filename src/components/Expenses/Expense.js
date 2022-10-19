7068b98fb7c75fad6ec1e6152e79866d68d27668
Gazalrafi <grafi7485>
2022-10-19 23:17:54 +0530

    another event commit

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -4,18 +4,28 @@ import './Expense.css';
 import ExpenseAmount from './ExpenseAmount.js';
 import Card from '../UI/Card.js';
 const Expense=(props)=>{
-   const [newtitle,setTitle]=useState(props.title);
+   const [newAmount,setNewAmount]=useState("")
+   
+   const [amount,setAmount]=useState(props.amount);
+   
    const clickHandler=()=>{
-    setTitle('new title');
+
+    setAmount(newAmount);
+   }
+   const changeHandler=(event)=>{
+
+    setNewAmount(event.target.value)
+
    }
     return(
         
         <Card className='expense-item'>
     
-        <div className='expense-item__title'><h1>{newtitle}</h1></div>
+        <div className='expense-item__title'><h1>{props.title}</h1></div>
         <ExpenseDate date={props.date} />
-        <ExpenseAmount amount={props.amount} />
+        <ExpenseAmount amount={amount} />
         <div className='expense-item__location'>{props.location}</div>
+        <input type="text" value={newAmount} onChange={changeHandler}/>
         <button onClick={clickHandler}>Click here to change title</button>
         </Card>
     );
