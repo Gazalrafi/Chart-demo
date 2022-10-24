069aea3dbbcfd78f559c1d009ae696e7ef856623
Gazalrafi <grafi7485>
2022-10-24 22:43:52 +0530

    my change in filter

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,29 +1,23 @@
-import React,{useState} from "react";
+import React from "react";
 import "./ExpensesFilter.css";
 const ExpensesFilter=(props)=>{
-        const [FilteredYear,setFilteredYear]=useState('');
-      
+        
         const FilterChangeHandler=(event)=>{
-        setFilteredYear(event.target.value);
+        props.onChangeFilter(event.target.value);
         };
-        const selectHandler=()=>{
-        const Year={
-            year:FilteredYear
-        }
-        props.onClickYear(Year);
-        console.log(Year);
-        setFilteredYear('2020')
-    }
+        
 return(
  <div>
-    <label className="expenses-filter"><h3>Select Year</h3></label>
-    <select className="expenses-select" value={selectHandler} onChange={FilterChangeHandler}>
+ <div className="expenses-filter">
+    <label ><h2>Filter by Year</h2></label>
+    <select  className="expenses-select" value={props.selected} onChange={FilterChangeHandler} >
     <option className="year-nineteen">2019</option>
     <option className="year-twenty">2020</option>
     <option className="year-twentyone">2021</option>
     <option className="year-twentytwo">2022</option>
     </select>
  </div>
+ </div>
 );
-}
+};
 export default ExpensesFilter;
--- src/components/Expenses/ExpensesMain.js
+++ src/components/Expenses/ExpensesMain.js
@@ -1,31 +1,39 @@
-import React from "react";
-import './ExpensesMain.css';    //
+import React,{useState} from "react";
+import './ExpensesMain.css';    
 import Expense from'./Expense.js';
 import Card from'../UI/Card.js';
 import ExpensesFilter from "./ExpensesFilter.js";
 
 const ExpensesMain=(props)=>{
+  const [FilteredYear,setFilteredYear]=useState('2020');
+      
   const saveYearHandler=(enteredYear)=>{
-    props.onAddYear(enteredYear);
+    setFilteredYear(enteredYear);
     console.log(enteredYear)
-  }
-  // const filteredExpenses=props.item.filter(expense=>{
-  //   return expense.date.getFullYear().toString()===saveYearHandler;
-  // })
+  };
+  const filteredExpenses=props.item.filter(expense=>{
+    return expense.date.getFullYear().toString()===FilteredYear;
+  })
 return (
+  <div>
   <Card className='expensesMain'>
-     <ExpensesFilter onSaveYear={saveYearHandler}/>
-    {   
-        props.item.map( expensemain=>(
+     <ExpensesFilter
+      selected={FilteredYear}
+      onChangeFilter={saveYearHandler}/>
+    {   filteredExpenses.map( expensemain=>(
          
                 < Expense
+                    key={expensemain.id}
                     date={expensemain.date}
                     title={expensemain.title}
                     amount={expensemain.amount}
                     location={expensemain.location}/>
             )
-        )
+    )
     }
-     </Card>)
+    
+     </Card>
+     </div>
+)
 }
 export default ExpensesMain;
